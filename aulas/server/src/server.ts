a2dcf52865b7d2b6b4a6dba436a372b83933b458
Ewerton Piccinini Spezia <ewertonps@pop-os.localdomain>
2020-08-07 18:06:59 -0300

    Adicionado arquivos de projeto ao git

---
+++ aulas/server/src/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import routes from './routes';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+//GET: Buscar ou listar uma informação
+//POST: Criar alguma nova informação
+//PUT: Atualizar uma informação existente
+//DELETE: Deletar uma informação existente
+
+// Corpo (Request Body): Dados para criação ou atualização de um registro
+// Route Params: Identificar qual recurso eu quero atualizar ou deletar
+// Query Params: Paginação, filtros, ordenação
+
+app.use(routes);
+
+app.listen(3333);
